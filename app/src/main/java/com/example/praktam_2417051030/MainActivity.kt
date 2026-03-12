6c43ebd24bc768170ac210a830932753ae2b026d
kiisann <fikisulistiawan@gmail.com>
2026-03-12 16:59:07 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam_2417051030/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051030/MainActivity.kt
@@ -2,20 +2,32 @@ package com.example.praktam_2417051030
 
 import Model.Todolist
 import Model.TodolistSource
-import android.R
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,11 +40,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrakTAM_2417051030Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    TodoScreen()
             }
         }
     }
@@ -59,10 +67,81 @@ fun Greeting(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun TodoScreen(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+        .padding(24.dp)
+    ) {
+        TodolistSource.dummyTodolist.forEach { todo ->
+            TodoDetailScreen(todo = todo)
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun TodoDetailScreen(modifier: Modifier = Modifier, todo: Todolist) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Card(modifier = Modifier) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(id = todo.imageRes),
+                    contentDescription = todo.kegiatan,
+                    modifier = Modifier.fillMaxWidth().height(200.dp)
+                        .padding(16.dp, 16.dp,16.dp,0.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(text = todo.kegiatan,
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = Color.Black,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = "Deadline: ${todo.deadline}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = "Prioritas: ${todo.prioritas}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = "Catatan: ${todo.catatan}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(text = "Status: ${todo.status}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black,
+                    modifier = Modifier.padding(16.dp, 0.dp, 0.dp, 16.dp)
+                )
+
+                Button(onClick = { },
+                    modifier = Modifier.fillMaxWidth().padding(16.dp, 0.dp, 16.dp, 16.dp)
+                ) { Text(text = "Selesai") }
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PrakTAM_2417051030Theme {
-        Greeting()
+        TodoScreen()
     }
 }
